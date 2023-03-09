0f1c0fddce31f52725f9d3cceabbc05290d0881f
Jacob Welsh <welshy92@outlook.com>
2023-03-09 16:55:28 +0000

    Set up templates

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Sidenav initialisation
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
