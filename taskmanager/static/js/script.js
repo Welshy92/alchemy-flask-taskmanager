3dc53e80c365b03ad2895a8d20a8c1345ecc655d
Jacob Welsh <welshy92@outlook.com>
2023-03-10 15:21:11 +0000

    Add displaying of tasks from database

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,19 @@ document.addEventListener('DOMContentLoaded', function() {
     // Sidenav initialisation
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
-  });
+
+    // Datepicker initialisation
+    let datepicker = document.querySelectorAll(".datepicker");
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    // Option initialisation
+    let selects = document.querySelectorAll("select");
+    M.FormSelect.init(selects);
+
+    // Collapsible initialisation
+    var collapse = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapse);
+});
